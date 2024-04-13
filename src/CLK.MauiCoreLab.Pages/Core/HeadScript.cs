2ad7bd1dc7b8f4944c70d808e2997fc2cc14bd33
clark159 <clark159@hotmail.com>
2024-04-13 21:12:56 +0800

    XXXXXXXXXXX

--- src/CLK.MauiCoreLab.Pages/Core/HeadScript.cs
+++ src/CLK.MauiCoreLab.Pages/Core/HeadScript.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Rendering;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Sections;
-using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Web;
 
 namespace CLK.MauiCoreLab.Pages
 {
