cb26d3a033738dc6cad111ba8a429a60639e91a3
clark159 <clark159@hotmail.com>
2024-04-13 21:03:22 +0800

    XXXXXXXXXXXXX

---
+++ src/CLK.MauiCoreLab.Pages/Core/HeadScript.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Sections;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace CLK.MauiCoreLab.Pages
+{
+    public sealed class HeadScript : ComponentBase
+    {
+        // Properties
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+
+        // Methods
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            // Build
+            builder.OpenComponent<SectionContent>(0);
+            builder.AddComponentParameter(1, nameof(SectionContent.SectionId), HeadScriptOutlet.SectionId);
+            builder.AddComponentParameter(2, nameof(SectionContent.ChildContent), this.ChildContent);
+            builder.CloseComponent();
+        }
+    }
+}
---
+++ src/CLK.MauiCoreLab.Pages/Core/HeadScriptOutlet.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Sections;
+
+namespace CLK.MauiCoreLab.Pages
+{
+    public sealed class HeadScriptOutlet : ComponentBase
+    {
+        // Constants
+        internal static readonly object SectionId = new();
+
+
+        // Methods
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            // Build
+            builder.OpenComponent<SectionOutlet>(0);
+            builder.AddComponentParameter(1, nameof(SectionOutlet.SectionId), SectionId);
+            builder.CloseComponent();
+        }
+    }
+}
