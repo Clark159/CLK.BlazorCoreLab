649ba016f6a19416a31c8d1d7db2d2fbd7cf2f9e
clark159 <clark159@hotmail.com>
2024-04-13 11:14:49 +0800

    Create CLK.MauiCoreLab.WebApp

---
+++ src/CLK.MauiCoreLab/CLK.MauiCoreLab/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLK.MauiCoreLab
+{
+    public class MessageService
+    {
+        // Methods
+        public string GetValue()
+        {
+            // Return
+            return "Hello World!";
+        }
+    }
+}
